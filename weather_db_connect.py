4dbd5155211497f58e1940000b3dad3511e3bd53
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-15 04:06:56 -0400

    Progressing very well

--- weather_db_connect.py
+++ weather_db_connect.py
@@ -1,9 +1,9 @@
 import json
 import psycopg2
-
-
 from dataclasses import dataclass
+from datetime import datetime as dt
 from typing import NamedTuple
+from datetime import datetime, timedelta
 from pathlib import Path
 
 class ForecastDB:
@@ -11,27 +11,26 @@ class ForecastDB:
         self.data = ForecastDB.load_json('Forecast_data.json')
         self.config = sql_params
         self.sql_connect(self.config)
-        
-    
+
     @staticmethod
     def load_json(file):
         return json.load(open(Path(__file__).parent.absolute() / file, encoding='utf-8'))
-    
+
     @staticmethod
-    def config_user(self):
+    def config_user():
         config = ForecastDB.load_json('config.json')
-        return self.sql_connect(config)
-    
+        return config
+
     def sql_connect(self, config_):
         global connection, cursor
-        
+
         @dataclass
         class SqlParams:
             host: str
             database: str
             user: str
             password: str
-        
+
         config = SqlParams(*config_)
 
         connection = psycopg2.connect(
@@ -41,55 +40,65 @@ class ForecastDB:
             password=config.password
         )
         cursor = connection.cursor()
+        self.create_tables()
         self.update_db(self.data)
+
+    def create_tables(self):
+        start_date = datetime.now().date()
         
+        class ParsedDate(NamedTuple):
+            year: int
+            month: int
+            day: int
+        
+        for day in range(15):
+            date = ParsedDate(*list(map(lambda i: int(i), self.data[day]['date'].split('-'))))
+            table_name = f"Day_{date.month}_{date.day}_{date.year}"
+            self.create_day_table(table_name)
+            for hour in range(24):
+                hour_table_name = f"{table_name}_Hour_{hour:02d}"
+                self.create_hour_table(hour_table_name)
+
+    def create_day_table(self, table_name):
+        
+        class SqlInfo(NamedTuple):
+            location: str
+            temperature: str
+            hourly: str
+            weatheremoji: str
+        
+        weather_db = SqlInfo(*open(Path.cwd() / 'weather_db.sql').read().split('\n'))
+        
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ("
+                       f"temperature_id SERIAL PRIMARY KEY,"
+                       f"location_id INTEGER REFERENCES Locations(location_id),"
+                       f"day DATE,"
+                       f"min_temp_cel DECIMAL(4,2),"
+                       f"min_temp_fah DECIMAL(4,2),"
+                       f"max_temp_cel DECIMAL(4,2),"
+                       f"max_temp_fah DECIMAL(4,2)"
+                       f")")
+
+    def create_hour_table(self, table_name):
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ("
+                       f"hourly_id SERIAL PRIMARY KEY,"
+                       f"temperature_id INTEGER REFERENCES Temperature(temperature_id),"
+                       f"hour TIME,"
+                       f"temp_cel DECIMAL(4,2),"
+                       f"temp_fah DECIMAL(4,2),"
+                       f"humidity INTEGER,"
+                       f"conditions VARCHAR(255)"
+                       f")")
+
     def update_db(self, data):
-        
-        global LocationInfo
-        class LocationInfo(NamedTuple):
-            arg1: float
-            arg2: float
-        
-        for day_data in data:
-            location_name = day_data['location']
-            coordinates = LocationInfo(arg1=day_data['coordinates']['longitude'],
-                                        arg2='latitude')
-            execute_first_query = 'INSERT INTO Locations (location_name, longitude, latitude, \
-                            coordinate) VALUES (%s %s %s %s) RETURNING location_id'
-            cursor.execute(execute_first_query, (location_name, coordinates.arg1, coordinates.arg2,
-                                        psycopg2.extensions.AsIs(f"POINT({coordinates.arg1} {coordinates.arg2})")))
-            locations_id = cursor.fetchone()[0]
-            
-            
-            min_temp = LocationInfo(arg1=day_data['min_temp']['Celcius'],
-                                    arg2=day_data['min_temp']['Fahrenheit'])
-            max_temp = LocationInfo(arg1=day_data['max_temp']['Celcius'],
-                                    arg2=day_data['max_temp']['Fahrenheit'])
-            execute_second_query = 'INSERT INTO Temperature (location_id, )'
-            
-            forecast_id = cursor.fetchone()[0]
-            
-            for hourly in day_data['hourly_data']:
-                hour = hourly['hour']
-                hourly_temp = LocationInfo(arg1=hourly['temperature']['Celcius'],
-                                            arg2=hourly['temperature']['Fahrenheit'])
-                humidity = hourly['humidity']
-                conditions = hourly['conditions']
-                emoji = LocationInfo(arg1=hourly['emoji']['Icon Code'],
-                                    arg2=hourly['emoji']['Decoded Bytes'])
-                execute_second_query = 'INSERT INTO '
-                
-            
-        
-        
-        return self.close_db()
-    
+        pass
+        # Update the database based on the data
+
     def close_db(self):
         connection.commit()
         cursor.close()
         connection.close()
-        return
-        
+
 
 def main():
     config = ForecastDB.load_json('config.json')
@@ -99,7 +108,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
