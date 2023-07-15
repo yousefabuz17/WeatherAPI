507be692ecea47bbb02e7111c2cedf08807a1768
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-14 23:55:15 -0400

    SQL DB seems completed

--- weather_db.sql
+++ weather_db.sql
@@ -7,17 +7,26 @@ CREATE TABLE Locations(
   coordinates POINT
 );
 
-CREATE TABLE Forecast (
+CREATE table Forecast(
     forecast_id SERIAL PRIMARY KEY
-    location_id INTEGER REFERENCES(Locations, location_id)
+    location_forecast_id INTEGER REFERENCES Locations(location_id)
     day DATE
-    min_temp FLOAT
-    max_temp FLOAT
+    min_temp_cel DECIMAL(4,2)
+    min_temp_fah DECIMAL(4,2)
+    max_temp_cel DECIMAL(4,2)
+    max_temp_fah DECIMAL(4,2)
 );
 
-CREATE TABLE hourly_data (
-    
-
+CREATE TABLE Hourly(
+    hourly_data_id SERIAL PRIMARY KEY
+    forecast_locaation_id INTEGER REFERENCES Forecast(location_forecast_id)
+    hour TIME
+    temp_cel DECIMAL(4,2)
+    temp_fah DECIMAL(4,2)
+    humidity INTEGER
+    conditions VARCHAR(255)
+    weather_icon_code VARCHAR(255)
+    bytes BYTEA
 );
 
 -- DROP DATABASE IF EXISTS postgres;
