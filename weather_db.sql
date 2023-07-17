b319388cf93e4f05fef03ee4d0d4e2a7fd1c7503
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-16 23:29:44 -0400

    SQL script updated

--- weather_db.sql
+++ weather_db.sql
@@ -1,33 +1,50 @@
-CREATE TABLE Locations(
+CREATE TABLE IF NOT EXISTS Locations (
     location_id SERIAL PRIMARY KEY,
     location_name VARCHAR(255),
-    longitude DECIMAL(5, 3),
-    latitude DECIMAL(5,3)
+    longitude DECIMAL(6, 4),
+    latitude DECIMAL(6, 4)
 );
 
-CREATE TABLE Temperature(
+CREATE TABLE IF NOT EXISTS Temperature (
     temperature_id SERIAL PRIMARY KEY,
-    location_id INTEGER REFERENCES Locations(location_id),
+    location_id INTEGER,
     day DATE,
-    min_temp_cel DECIMAL(4,2),
-    min_temp_fah DECIMAL(4,2),
-    max_temp_cel DECIMAL(4,2),
-    max_temp_fah DECIMAL(4,2)
+    min_temp_cel DECIMAL(5, 2),
+    min_temp_fah DECIMAL(5, 2),
+    max_temp_cel DECIMAL(5, 2),
+    max_temp_fah DECIMAL(5, 2),
+    FOREIGN KEY (location_id) REFERENCES Locations (location_id)
 );
 
-CREATE TABLE Hourly(
+CREATE TABLE IF NOT EXISTS Hourly (
     hourly_id SERIAL PRIMARY KEY,
-    temperature_id INTEGER REFERENCES Temperature(temperature_id),
+    temperature_id INTEGER,
     hour TIME,
-    temp_cel DECIMAL(4,2),
-    temp_fah DECIMAL(4,2),
+    temp_cel DECIMAL(5, 2),
+    temp_fah DECIMAL(5, 2),
     humidity INTEGER,
     conditions VARCHAR(255),
-    UNIQUE (temperature_id)
+    FOREIGN KEY (temperature_id) REFERENCES Temperature (temperature_id)
 );
 
-CREATE Table WeatherEmoji(
+CREATE TABLE IF NOT EXISTS WeatherEmoji (
     emoji_id SERIAL PRIMARY KEY,
     icon_code VARCHAR(255),
     bytes BYTEA
 );
+
+INSERT INTO Locations (location_name, longitude, latitude)
+VALUES (%s, %s, %s)
+RETURNING location_id;
+
+INSERT INTO Temperature (location_id, day, min_temp_cel, min_temp_fah, max_temp_cel, max_temp_fah)
+VALUES (%s, %s, %s, %s, %s, %s)
+RETURNING temperature_id;
+
+INSERT INTO Hourly (temperature_id, hour, temp_cel, temp_fah, humidity, conditions)
+VALUES (%s, %s, %s, %s, %s, %s)
+RETURNING hourly_id;
+
+INSERT INTO WeatherEmoji (icon_code, bytes)
+VALUES (%s, %s)
+RETURNING emoji_id;
