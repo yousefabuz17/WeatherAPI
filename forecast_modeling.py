1d269a7b5ec51a111ce7ea54aa925f06e0b3766a
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-20 01:15:22 -0400

    Grouping classes working. Might use chain

--- forecast_modeling.py
+++ forecast_modeling.py
@@ -32,6 +32,14 @@ class SQLFetch(NamedTuple):
 class Args:
     arg1: str
     arg2: str
+    
+    # def __str__(self):
+    #     try:
+    #         df = pd.DataFrame(self.arg1, columns=self.arg2)
+    #         return str(df)
+    #     except (AttributeError, TypeError):
+    #         return
+
 
 
 class DBConnect:
@@ -45,18 +53,18 @@ class DBConnect:
         self.database = None
         self.validator()
     
-    def __str__(self) -> str: #**Prints full database into a DataFrame
-        try:
-            rows = self.database.arg1
-            columns = self.database.arg2
-            df = pd.DataFrame(rows, columns=columns)
-            pd.set_option('display.max_columns', None)
-            return df.__repr__() # For demonstration purposes
-        except (AttributeError, FileNotFoundError) as e: 
-            return self.__string(e)
+    # def __str__(self) -> str: #**Prints full database into a DataFrame
+    #     try:
+    #         rows = self.database.arg1
+    #         columns = self.database.arg2
+    #         df = pd.DataFrame(rows, columns=columns)
+    #         pd.set_option('display.max_columns', None)
+    #         return df.__repr__() # For demonstration purposes
+    #     except (AttributeError, FileNotFoundError) as e: 
+    #         return self.__string(e)
     
-    def __repr__(self) -> str:
-        return self.__str__()
+    # def __repr__(self) -> str:
+    #     return self.__str__()
 
     def __del__(self):
         if self.cursor:
@@ -66,7 +74,7 @@ class DBConnect:
         try:
             if self.database is not None and hasattr(config, 'database'):
                 return self.database
-            raise AttributeError(self.__string())
+            raise AttributeError(self.__string())   #!Remove later
         except (RecursionError, NameError) as e:
             return self.__string(e)
     
@@ -137,101 +145,110 @@ class DBConnect:
             raise e
             return 'Invalid Input'
 
-
+#TODO: Make class to retrieve all values in specific columns
 
 class GroupBy:
-    database = DBConnect()
+    database = Args(arg1=DBConnect()._.arg1, arg2=DBConnect()._.arg2)
 
     @classmethod
-    def __str__(cls, df: pd.DataFrame) -> str:
-        return df.__repr__()
-
-    @classmethod
-    def __call__(cls, *args, **kwargs):
-        return cls.chain_conditions(*args, **kwargs)
+    def __str__(cls,):
+        return ''
     
     @classmethod
-    def location_id(cls, id_: int=None, db: DBConnect=None, viewtable: bool=False) -> DBConnect:
+    def reset_pd_to_args(cls, pd_):
+        rows = pd_.values.tolist()
+        columns = pd_.columns.tolist()
+        data_reset = Args(arg1=rows, arg2=columns)
+        return data_reset
+    
+    @classmethod
+    def location_id(cls, id_: int|str=None):
         try:
-            if db is None:
-                db = cls.database
-            data = db.group_locations('location_id', id_)
-
-            if isinstance(data.arg1, pd.DataFrame):
-                df = data.arg1.copy()
+            data = DBConnect().group_locations('location_id', id_)
+            return data
+        except (psycopg.errors.InvalidTextRepresentation, psycopg.errors.SyntaxError):
+            return 'Invalid input'
+    
+    @classmethod
+    def filter_by_day(cls, day, data=None):
+        try:
+            if data is None:
+                data = pd.DataFrame(cls.database.arg1, columns=cls.database.arg2)
+                filtered_data = data[data['day'] == day]
+                return GroupBy.reset_pd_to_args(filtered_data)
+            else:
+                data = pd.DataFrame(data.arg1, columns=data.arg2)
+                filtered_data = data[data['day'] == day]
+                return GroupBy.reset_pd_to_args(filtered_data)
+        except (psycopg.errors.InvalidTextRepresentation, psycopg.errors.SyntaxError):
+            return 'Invalid input'
+    
+    @classmethod
+    def filter_by_condition(cls, condition, data=None):
+        try:
+            if data is None:
+                df = pd.DataFrame(cls.database.arg1, columns=cls.database.arg2)
+                filtered_data = df[df['condition'] == condition]
+                return GroupBy.reset_pd_to_args(filtered_data)
             else:
                 df = pd.DataFrame(data.arg1, columns=data.arg2)
-
-            if viewtable:
-                return df
-            else:
-                return db
-
-            # return db
-        except psycopg.errors.InvalidTextRepresentation:
-            return 'Invalid input'
-
-    @classmethod
-    def condition_type(cls, type_: str=None, value: str=None, db: DBConnect=None, viewtable: bool=False) -> DBConnect:
-        if db is None:
-            db = cls.database
-            
-        if isinstance(db._.arg1, pd.DataFrame):
-            df = db._.arg1.copy()
-        else:
-            df = pd.DataFrame(db._.arg1, columns=db._.arg2)
+                filtered_data = df[df['condition'] == condition]
+                return GroupBy.reset_pd_to_args(filtered_data)
         
-        df = df.loc[df[type_] == value, db._.arg2]
-        if viewtable:
-            db = df
-            print(db)
-            return db
-        else:
-            df = df.loc[df[type_] == value, db._.arg2]
-            db = df
-            return db
-
-        return db
-
+        except (psycopg.errors.InvalidTextRepresentation, psycopg.errors.SyntaxError):
+            return 'Invalid input' 
+    
     @classmethod
-    def reset(cls, _) -> DBConnect:
-        return cls.database
-
+    def filter_by_temperature(cls, min_temp, max_temp, data=None):
+        try:
+            if data is None:
+                df = pd.DataFrame(cls.database.arg1, columns=cls.database.arg2)
+                filtered_data = df[(df['temp_fah'] >= min_temp) & (df['temp_fah'] <= max_temp)]
+                return GroupBy.reset_pd_to_args(filtered_data)
+            else:
+                df = pd.DataFrame(data.arg1, columns=data.arg2)
+                filtered_data = df[(df['temp_fah'] >= min_temp) & (df['temp_fah'] <= max_temp)]
+                return GroupBy.reset_pd_to_args(filtered_data)
+        
+        except (psycopg.errors.InvalidTextRepresentation, psycopg.errors.SyntaxError):
+            return 'Invalid input' 
+    
     @classmethod
-    def chain_conditions(cls, type_: str=None, db: DBConnect=None, **conditions) -> DBConnect:
-        if db is None:
-            db = cls.database
+    def filter_by_humidity(cls, min_humidity, max_humidity, data=None):
+        try:
+            if data is None:
+                df = pd.DataFrame(cls.database.arg1, columns=cls.database.arg2)
+                filtered_data = df[(df['humidity'] >= min_humidity) & (df['humidity'] <= max_humidity)]
+                return GroupBy.reset_pd_to_args(filtered_data)
+            else:
+                df = pd.DataFrame(data.arg1, columns=data.arg2)
+                filtered_data = df[(df['humidity'] >= min_humidity) & (df['humidity'] <= max_humidity)]
+                return GroupBy.reset_pd_to_args(filtered_data)
+        
+        except (psycopg.errors.InvalidTextRepresentation, psycopg.errors.SyntaxError):
+            return 'Invalid input' 
 
-        if isinstance(db, pd.DataFrame):
-            df = db.copy()
-        else:
-            df = pd.DataFrame(db._.arg1, columns=db._.arg2)
 
-        for condition_type, value in conditions.items():
-            df = df.loc[df[condition_type] == value]
-        return db
-
+    # @classmethod
+    # def __call__(cls, *args, **kwargs):
+    #     return cls.chain_conditions(*args, **kwargs)
 
 def main():
-    #location_name, day, humidity, condition
-    db = DBConnect()    #!'db._.arg1'
-    grouper = GroupBy
-    loc_group = grouper.location_id
-    con_group = grouper.condition_type
-    reset = grouper.reset
-    d = loc_group(4, viewtable=False)
-    print(d)
-    # df_conditioned = con_group(type_='condition', db=con_group(type_='humidity', value=73), value='Shower Rain')
+    database = DBConnect()    # ! 'db._.arg1'
+    db = Args(arg1=database._.arg1, arg2=database._.arg2)
+    groupby = GroupBy()
+    loc = groupby.location_id
+    cond = groupby.filter_by_condition
+    temper = groupby.filter_by_temperature
+    hum = groupby.filter_by_humidity
+    day_ = groupby.filter_by_day
     
-    # b = con_group('Shower Rain', d, viewtable=False)
-    # df = pd.DataFrame(d.arg1, columns=d.arg2)
-    # print(df.loc[df['temp_fah'] == 90.500, d.arg2])
+    
+    d = hum(60, 100, data=day_('07/31/2023', data=cond(condition='Shower Rain', data=db)))
+    #d = hum(60, 100) This also works
+    df = pd.DataFrame(d.arg1, columns=d.arg2)
+    print(df)
 
-    # first_table = loc_group(2)
-    
-    # second_table = con_group('Scattered Clouds', loc_group(1), viewtable=False)
-    # df = pd.DataFrame(second_table.arg1, columns=second_table.arg2)
-    # print(first_table)
 
 if __name__ == '__main__':
     main()
