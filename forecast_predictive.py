337cccc841b6831e7983023031e908a3b6666650
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-20 18:09:58 -0400

    Initial commit

---
+++ forecast_predictive.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+from groupby_db import DBConnect, GroupBy
+
