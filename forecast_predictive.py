be6f63067378b85562971c5a2266a290fec2dd55
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-22 15:17:40 -0400

    r2_score implemented

--- forecast_predictive.py
+++ forecast_predictive.py
@@ -2,9 +2,10 @@ import warnings
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+
 from groupby_db import DBConnect, GroupBy
 
 
@@ -21,9 +22,9 @@ class WeatherManager:
         num_locations = len(self.database.get_locations)
         for i in range(1, num_locations):
             df = pd.DataFrame(loc(i).arg1, columns=loc(i).arg2)
-            self.regression_model(df)
+            self.regression_model_(df)
     
-    def regression_model(self, data):
+    def regression_model_(self, data):
         location = reset(data).arg1[0][0]
         data.columns = data.columns.str.strip()
         data['hour'] = data['hour'].apply(lambda t: t.strftime('%H:%M:%S'))
@@ -57,6 +58,7 @@ class WeatherManager:
             'max_depth': [None, 5, 10, 15],
             'min_samples_split': [2, 5, 10]
         }
+        
         regression_model = RandomForestRegressor(random_state=42)
         grid_search_regression = GridSearchCV(regression_model, param_grid=param_grid_regression, cv=5, n_jobs=-1)
         grid_search_regression.fit(X_temp_train_scaled, y_temp_train)
@@ -64,7 +66,8 @@ class WeatherManager:
 
         y_temp_pred = best_regression_model.predict(X_temp_test_scaled)
         mse_temp = mean_squared_error(y_temp_test, y_temp_pred)
-
+        r2_temp = r2_score(y_temp_test, y_temp_pred)
+        
         param_grid_classification = {
             'n_estimators': [100, 200, 300],
             'max_depth': [None, 5, 10, 15],
@@ -77,18 +80,17 @@ class WeatherManager:
 
         y_cond_pred = best_classification_model.predict(X_cond_test)
         accuracy_cond = accuracy_score(y_cond_test, y_cond_pred)
-        accuracy_percentage = accuracy_cond * 100
         
         print(f'\nResults for {location}:')
-        print(f'\tTemperature Regression Mean Squared Error: {mse_temp}')
-        print(f'\tWeather Condition Classification Accuracy: {accuracy_percentage:.2f}%\n\n')
-
+        print(f'\tTemperature Regression Mean Squared Error: {mse_temp:.3f}')
+        print(f'\tTemperature Regression R-squared: {r2_temp:.3f}')
+        print(f'\tWeather Condition Classification Accuracy: {accuracy_cond * 100:.2f}%\n\n')
+    
 def main():
     global loc, reset
     groupby = GroupBy()
     reset = groupby._reset
     loc = groupby.location_id
-    
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=UserWarning)
         WeatherManager(DBConnect())
